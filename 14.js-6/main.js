46191ed6b91e21cdcb6b54c39e2badc90c97392c
MelmQuty <towa8547@gmail.com>
2021-03-29 13:25:04 +0300

    1

--- 14.js-6/main.js
+++ 14.js-6/main.js
@@ -154,138 +154,138 @@
 // =============================================
 // =============================================
 
-let cars = [
-  {
-    producer: "subaru",
-    model: "wrx",
-    year: 2010,
-    color: "blue",
-    type: "sedan",
-    engine: "ej204x",
-    volume: 2,
-    power: 400,
-  },
-  {
-    producer: "subaru",
-    model: "legacy",
-    year: 2007,
-    color: "silver",
-    type: "sedan",
-    engine: "ez30",
-    volume: 3,
-    power: 250,
-  },
-  {
-    producer: "subaru",
-    model: "tribeca",
-    year: 2011,
-    color: "white",
-    type: "jeep",
-    engine: "ej20",
-    volume: 2,
-    power: 300,
-  },
-  {
-    producer: "subaru",
-    model: "leone",
-    year: 1998,
-    color: "yellow",
-    type: "sedan",
-    engine: "ez20x",
-    volume: 2,
-    power: 140,
-  },
-  {
-    producer: "subaru",
-    model: "impreza",
-    year: 2014,
-    color: "red",
-    type: "sedan",
-    engine: "ej204x",
-    volume: 2,
-    power: 200,
-  },
-  {
-    producer: "subaru",
-    model: "outback",
-    year: 2014,
-    color: "red",
-    type: "hachback",
-    engine: "ej204",
-    volume: 2,
-    power: 165,
-  },
-  {
-    producer: "bmw",
-    model: "115",
-    year: 2013,
-    color: "red",
-    type: "hachback",
-    engine: "f15",
-    volume: 1.5,
-    power: 120,
-  },
-  {
-    producer: "bmw",
-    model: "315",
-    year: 2010,
-    color: "white",
-    type: "sedan",
-    engine: "f15",
-    volume: 1.5,
-    power: 120,
-  },
-  {
-    producer: "bmw",
-    model: "650",
-    year: 2009,
-    color: "black",
-    type: "coupe",
-    engine: "f60",
-    volume: 6,
-    power: 350,
-  },
-  {
-    producer: "bmw",
-    model: "320",
-    year: 2012,
-    color: "red",
-    type: "sedan",
-    engine: "f20",
-    volume: 2,
-    power: 180,
-  },
-  {
-    producer: "mercedes",
-    model: "e200",
-    year: 1990,
-    color: "silver",
-    type: "sedan",
-    engine: "eng200",
-    volume: 2,
-    power: 180,
-  },
-  {
-    producer: "mercedes",
-    model: "e63",
-    year: 2017,
-    color: "yellow",
-    type: "sedan",
-    engine: "amg63",
-    volume: 3,
-    power: 400,
-  },
-  {
-    producer: "mercedes",
-    model: "c250",
-    year: 2017,
-    color: "red",
-    type: "sedan",
-    engine: "eng25",
-    volume: 2.5,
-    power: 230,
-  },
-];
+// let cars = [
+//   {
+//     producer: "subaru",
+//     model: "wrx",
+//     year: 2010,
+//     color: "blue",
+//     type: "sedan",
+//     engine: "ej204x",
+//     volume: 2,
+//     power: 400,
+//   },
+//   {
+//     producer: "subaru",
+//     model: "legacy",
+//     year: 2007,
+//     color: "silver",
+//     type: "sedan",
+//     engine: "ez30",
+//     volume: 3,
+//     power: 250,
+//   },
+//   {
+//     producer: "subaru",
+//     model: "tribeca",
+//     year: 2011,
+//     color: "white",
+//     type: "jeep",
+//     engine: "ej20",
+//     volume: 2,
+//     power: 300,
+//   },
+//   {
+//     producer: "subaru",
+//     model: "leone",
+//     year: 1998,
+//     color: "yellow",
+//     type: "sedan",
+//     engine: "ez20x",
+//     volume: 2,
+//     power: 140,
+//   },
+//   {
+//     producer: "subaru",
+//     model: "impreza",
+//     year: 2014,
+//     color: "red",
+//     type: "sedan",
+//     engine: "ej204x",
+//     volume: 2,
+//     power: 200,
+//   },
+//   {
+//     producer: "subaru",
+//     model: "outback",
+//     year: 2014,
+//     color: "red",
+//     type: "hachback",
+//     engine: "ej204",
+//     volume: 2,
+//     power: 165,
+//   },
+//   {
+//     producer: "bmw",
+//     model: "115",
+//     year: 2013,
+//     color: "red",
+//     type: "hachback",
+//     engine: "f15",
+//     volume: 1.5,
+//     power: 120,
+//   },
+//   {
+//     producer: "bmw",
+//     model: "315",
+//     year: 2010,
+//     color: "white",
+//     type: "sedan",
+//     engine: "f15",
+//     volume: 1.5,
+//     power: 120,
+//   },
+//   {
+//     producer: "bmw",
+//     model: "650",
+//     year: 2009,
+//     color: "black",
+//     type: "coupe",
+//     engine: "f60",
+//     volume: 6,
+//     power: 350,
+//   },
+//   {
+//     producer: "bmw",
+//     model: "320",
+//     year: 2012,
+//     color: "red",
+//     type: "sedan",
+//     engine: "f20",
+//     volume: 2,
+//     power: 180,
+//   },
+//   {
+//     producer: "mercedes",
+//     model: "e200",
+//     year: 1990,
+//     color: "silver",
+//     type: "sedan",
+//     engine: "eng200",
+//     volume: 2,
+//     power: 180,
+//   },
+//   {
+//     producer: "mercedes",
+//     model: "e63",
+//     year: 2017,
+//     color: "yellow",
+//     type: "sedan",
+//     engine: "amg63",
+//     volume: 3,
+//     power: 400,
+//   },
+//   {
+//     producer: "mercedes",
+//     model: "c250",
+//     year: 2017,
+//     color: "red",
+//     type: "sedan",
+//     engine: "eng25",
+//     volume: 2.5,
+//     power: 230,
+//   },
+// ];
 
 // Відфільтрувати масив за наступними крітеріями :
 
@@ -340,86 +340,86 @@ let cars = [
 // console.log(carFilter);
 
 // - взять слдующий массив
-let usersWithAddress = [
-  {
-    id: 1,
-    name: "vasya",
-    age: 31,
-    status: false,
-    address: { city: "Lviv", street: "Shevchenko", number: 16 },
-  },
-  {
-    id: 2,
-    name: "petya",
-    age: 30,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 1 },
-  },
-  {
-    id: 3,
-    name: "kolya",
-    age: 29,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 121 },
-  },
-  {
-    id: 4,
-    name: "olya",
-    age: 28,
-    status: false,
-    address: { city: "Lviv", street: "Shevchenko", number: 90 },
-  },
-  {
-    id: 5,
-    name: "max",
-    age: 30,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 115 },
-  },
-  {
-    id: 6,
-    name: "anya",
-    age: 31,
-    status: false,
-    address: { city: "Lviv", street: "Shevchenko", number: 2 },
-  },
-  {
-    id: 7,
-    name: "oleg",
-    age: 28,
-    status: false,
-    address: { city: "Lviv", street: "Shevchenko", number: 22 },
-  },
-  {
-    id: 8,
-    name: "andrey",
-    age: 29,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 43 },
-  },
-  {
-    id: 9,
-    name: "masha",
-    age: 30,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 12 },
-  },
-  {
-    id: 10,
-    name: "olya",
-    age: 31,
-    status: false,
-    address: { city: "Lviv", street: "Shevchenko", number: 16 },
-  },
-  {
-    id: 11,
-    name: "max",
-    age: 31,
-    status: true,
-    address: { city: "Lviv", street: "Shevchenko", number: 121 },
-  },
-];
-console.log(usersWithAddress);
+// let usersWithAddress = [
+//   {
+//     id: 1,
+//     name: "vasya",
+//     age: 31,
+//     status: false,
+//     address: { city: "Lviv", street: "Shevchenko", number: 16 },
+//   },
+//   {
+//     id: 2,
+//     name: "petya",
+//     age: 30,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 1 },
+//   },
+//   {
+//     id: 3,
+//     name: "kolya",
+//     age: 29,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 121 },
+//   },
+//   {
+//     id: 4,
+//     name: "olya",
+//     age: 28,
+//     status: false,
+//     address: { city: "Lviv", street: "Shevchenko", number: 90 },
+//   },
+//   {
+//     id: 5,
+//     name: "max",
+//     age: 30,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 115 },
+//   },
+//   {
+//     id: 6,
+//     name: "anya",
+//     age: 31,
+//     status: false,
+//     address: { city: "Lviv", street: "Shevchenko", number: 2 },
+//   },
+//   {
+//     id: 7,
+//     name: "oleg",
+//     age: 28,
+//     status: false,
+//     address: { city: "Lviv", street: "Shevchenko", number: 22 },
+//   },
+//   {
+//     id: 8,
+//     name: "andrey",
+//     age: 29,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 43 },
+//   },
+//   {
+//     id: 9,
+//     name: "masha",
+//     age: 30,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 12 },
+//   },
+//   {
+//     id: 10,
+//     name: "olya",
+//     age: 31,
+//     status: false,
+//     address: { city: "Lviv", street: "Shevchenko", number: 16 },
+//   },
+//   {
+//     id: 11,
+//     name: "max",
+//     age: 31,
+//     status: true,
+//     address: { city: "Lviv", street: "Shevchenko", number: 121 },
+//   },
+// ];
+// console.log(usersWithAddress);
 
 // -- отсортировать его по id пользователей
 // let arrUser = JSON.parse(JSON.stringify(usersWithAddress));
@@ -495,7 +495,99 @@ console.log(usersWithAddress);
 // ======ДОПОЛНИТЕЛЬНО========
 // ===========================
 
-let car = [];
+// let cars = [
+//   {
+//     producer: "subaru",
+//     owner: { name: "Mark", age: 23, exp: 5 },
+//     year: 2010,
+//     power: 400,
+//     price: 28500,
+//   },
+//   {
+//     producer: "subaru",
+//     owner: { name: "Dinasour", age: 26, exp: 10 },
+//     year: 2007,
+//     power: 250,
+//     price: 26500,
+//   },
+//   {
+//     producer: "subaru",
+//     owner: { name: "Shina", age: 23, exp: 7 },
+//     year: 2011,
+//     power: 300,
+//     price: 24500,
+//   },
+//   {
+//     producer: "subaru",
+//     owner: { name: "Misha", age: 22, exp: 2 },
+//     year: 1998,
+//     power: 140,
+//     price: 25500,
+//   },
+//   {
+//     producer: "subaru",
+//     owner: { name: "Liza", age: 28, exp: 4 },
+//     year: 2014,
+//     power: 200,
+//     price: 23500,
+//   },
+//   {
+//     producer: "bmw",
+//     owner: { name: "Ralf", age: 27, exp: 4 },
+//     year: 2013,
+//     power: 120,
+//     price: 21500,
+//   },
+//   {
+//     producer: "bmw",
+//     owner: { name: "Rebekka", age: 29, exp: 8 },
+//     year: 2010,
+//     power: 120,
+//     price: 20500,
+//   },
+//   {
+//     producer: "bmw",
+//     owner: { name: "Melania", age: 26, exp: 3 },
+//     year: 2009,
+//     power: 350,
+//     price: 19500,
+//   },
+//   {
+//     producer: "mercedes",
+//     owner: { name: "Alina", age: 30, exp: 3 },
+//     year: 1990,
+//     power: 180,
+//     price: 17500,
+//   },
+//   {
+//     producer: "mercedes",
+//     owner: { name: "Volodya", age: 31, exp: 2 },
+//     price: 16500,
+//     year: 2017,
+//     power: 400,
+//   },
+// ];
+
+// let newCar = JSON.parse(JSON.stringify(cars));
+// let allPrice = 0;
+
+// for (let i = 0; i < newCar.length; i += 2) {
+//   (newCar[i].power += newCar[i].power * 0.1);
+//   (newCar[i].price += newCar[i].price * 0.05);
+// }
+// for (let i = 0; i < newCar.length; i++) {
+//   if (newCar[i].owner.exp < 5 && newCar[i].owner.age > 25) {
+//     newCar[i].owner.exp += 1;
+//   }
+//   allPrice += newCar[i].price;
+// }
+
+// console.log(allPrice);
+
+// console.log(cars);
+// console.log(newCar);
+
+
 // Створити обєкт автомобіля з полями:
 // Марка автомобля, потужність двигуна, власник, ціна, рік випуску.
 // Власник автомобіля теж має бути обєкт, у якого є поля
@@ -507,6 +599,8 @@ let car = [];
 // Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
 // Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
 // Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
+
+
 
 // Задача: дан отсортированный по возрастанию массив целых чисел. Необходимо вернуть наименьший и наибольший индекс заданного элемента.
 // Входные данные: arr — массив целых чисел значения которых по модулю не больше 10. Размер массива не более 10 элементов.
