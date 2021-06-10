635558ef2d3da09713083549c88a54ce1f30fb97
MelmQuty <towa8547@gmail.com>
2021-06-10 11:32:21 +0300

    commit

---
+++ angular/ang2/ang2/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+let routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {
+    path: 'users', loadChildren: () => import('./modules/user/user.module').then(m=>m.UserModule)},
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/ang2/ang2/src/app/modules/user/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private url = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private httpClient: HttpClient) {
+    
+  }
+  
+
+  getUsers(): Observable<any[]> {
+    return this.httpClient.get<any[]>(this.url);
+  }
+}
+
+
